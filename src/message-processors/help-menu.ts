ce6a286024f46c05f22b928824221f79f028525d
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2023-04-30 23:29:57 -0300

    :sparkles: Help menu and processor description

---
+++ src/message-processors/help-menu.ts
@@ -0,0 +1,16 @@
+import {Client} from "@open-wa/wa-automate";
+import {Message} from "@open-wa/wa-automate/dist/api/model/message";
+import {from, Observable} from "rxjs";
+import {msgProcessors} from "../message.routing";
+
+export function helpMenu(waClient: Client, message: Message, enableQuotedMessage: boolean = false): Observable<any> | Observable<never> {
+    if (enableQuotedMessage && message?.hasOwnProperty('quotedMsg')) {
+        message = message.quotedMsg as Message;
+    }
+
+    let helpText = `Lista de utilitários: \n\n`;
+    msgProcessors.forEach(p => {
+        helpText += `${p.description} \n`
+    })
+    return from(waClient.reply(message.chatId, `${helpText}`, message.id, true));
+}
--- src/message.routing.ts
+++ src/message.routing.ts
@@ -3,18 +3,27 @@ import {Message} from "@open-wa/wa-automate/dist/api/model/message";
 import {messageToFig} from "./message-processors/sticker-generator";
 import {passeiDiretoUrlDownload} from "./message-processors/passei-direto-downloader";
 import {Client} from "@open-wa/wa-automate";
+import {helpMenu} from "./message-processors/help-menu";
 
 export const msgProcessors: {
     msgFunc: (...args: any[]) => Observable<any>,
+    description: string,
     textEquals?: string,
     urlIncludes?: string,
 }[] = [
     {
+        textEquals: 'ajuda!',
+        msgFunc: (waClient: Client, m: Message) => helpMenu(waClient, m),
+        description: '⁉️ Ajuda: Envie "ajuda!" para exibir este menu'
+    },
+    {
         textEquals: 'fig',
         msgFunc: (waClient: Client, m: Message) => messageToFig(waClient, m),
+        description: '🥸 Figurinhas: Envie ou Responda uma imagem ou gif com o texto "fig", e ela será transformada em figurinha'
     },
     {
         urlIncludes: 'www.passeidireto.com',
-        msgFunc: (waClient: Client, m: Message, url: string) => passeiDiretoUrlDownload(waClient, m, url)
+        msgFunc: (waClient: Client, m: Message, url: string) => passeiDiretoUrlDownload(waClient, m, url),
+        description: '✏️ Passei Direto: Envie uma mensagem com links de documentos do PasseiDireto.com e eles serão baixados e enviados'
     },
 ]
